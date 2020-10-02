ec513ec8b10bd86d018a7f8bafe601fcae1ba153
Wlad <wlad@gmail.com>
2020-10-02 14:16:08 +0300

    Initial Commit

---
+++ try2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  try2
+//
+//  Created by student on 02.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
